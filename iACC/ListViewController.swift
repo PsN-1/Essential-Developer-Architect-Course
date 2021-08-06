5e9a15c315d563913e66a56585471f4f534acc40
Pedro Neto <pedro.n@db1.com.br>
2021-08-06 09:27:27 -0300

    Move ItemViewModel creation to the handleAPIResult method

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-    var items = [Any]()
+    var items = [ItemViewModel]()
     
     var retryCount = 0
     var maxRetryCount = 0
@@ -107,7 +107,19 @@ class ListViewController: UITableViewController {
                 }
             }
             
-            self.items = filteredItems
+            self.items = filteredItems.map { item in
+                ItemViewModel(item, longDateStyle: longDateStyle) { [weak self] in
+                    if let friend = item as? Friend {
+                        self?.select(friend: friend)
+                    } else if let card = item as? Card {
+                        self?.select(card: card)
+                    } else if let transfer = item as? Transfer {
+                        self?.select(transfer: transfer)
+                    } else {
+                        fatalError("unknown item: \(item)")
+                    }
+                }
+            }
             self.refreshControl?.endRefreshing()
             self.tableView.reloadData()
             
@@ -126,7 +138,11 @@ class ListViewController: UITableViewController {
                     DispatchQueue.mainAsyncIfNeeded {
                         switch result {
                         case let .success(items):
-                            self?.items = items
+                            self?.items = items.map { item in
+                                ItemViewModel(friend: item, selection: { [weak self] in
+                                        self?.select(friend: item)
+                                })
+                            }
                             self?.tableView.reloadData()
                             
                         case let .failure(error):
@@ -153,37 +169,28 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = items[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "ItemCell")
-        
-        let vm = ItemViewModel(item, longDateStyle: longDateStyle)
-        cell.configure(vm)
+        cell.configure(item)
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let item = items[indexPath.row]
-        if let friend = item as? Friend {
-            select(friend: friend)
-        } else if let card = item as? Card {
-            select(card: card)
-        } else if let transfer = item as? Transfer {
-            select(transfer: transfer)
-        } else {
-            fatalError("unknown item: \(item)")
-        }
+        item.select()
     }
 }
 
 struct ItemViewModel {
     let title: String
     let subtitle: String
+    let select: () -> Void
     
-    init(_ item: Any, longDateStyle: Bool) {
+    init(_ item: Any, longDateStyle: Bool, selection: @escaping () -> Void) {
         if let friend = item as? Friend {
-            self.init(friend: friend)
+            self.init(friend: friend, selection: selection)
         } else if let card = item as? Card {
-            self.init(card: card)
+            self.init(card: card, selection: selection)
         } else if let transfer = item as? Transfer {
-            self.init(transfer: transfer, longDateStyle: longDateStyle)
+            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
         } else {
             fatalError("unknown item: \(item)")
         }
@@ -191,21 +198,23 @@ struct ItemViewModel {
 }
 
 extension ItemViewModel {
-    init(friend: Friend) {
+    init(friend: Friend, selection: @escaping () -> Void) {
         title = friend.name
         subtitle = friend.phone
+        select = selection
     }
 }
 
 extension ItemViewModel {
-    init(card: Card) {
+    init(card: Card, selection: @escaping () -> Void) {
         title = card.number
         subtitle  = card.holder
+        select = selection
     }
 }
 
 extension ItemViewModel {
-    init(transfer: Transfer, longDateStyle: Bool) {
+    init(transfer: Transfer, longDateStyle: Bool, selection: @escaping () -> Void) {
         let numberFormatter = Formatters.number
         numberFormatter.numberStyle = .currency
         numberFormatter.currencyCode = transfer.currencyCode
@@ -223,7 +232,7 @@ extension ItemViewModel {
             dateFormatter.timeStyle = .short
             subtitle = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
         }
-        
+        select = selection
     }
 }
 
